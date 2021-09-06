774e65e7b0b7564c1e7bb9f07c643c5370fad268
Dario Ramos <52785865+darks000@users.noreply.github.com>
2021-09-05 21:16:01 -0500

    conexion a bd pt1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
+import { MensajeController } from './Controller/mensaje.controller';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),],
+  controllers: [
+    MensajeController, AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
